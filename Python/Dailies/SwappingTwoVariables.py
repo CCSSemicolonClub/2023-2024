37eb45342bee51a9df8f6d5b5618b3676c594388
ist00dent <119123891+ist00dent@users.noreply.github.com>
2023-10-08 16:50:38 +0800

    first task advance mode

---
+++ Python/Dailies/SwappingTwoVariables.py
@@ -0,0 +1,22 @@
+def swap(arr):
+    #Swap the values of x and y without using a temporary variable
+    arr[0], arr[1] = arr[1], arr[0]
+
+
+#Declare variables 
+x = 5 
+y = 10 
+arr = [x,y]
+
+#Call the swapping function to swap the values of x and y
+swap (arr) 
+
+# Now, we call the 'swap' function, passing our 'arr' list as an argument.
+# Inside the 'swap' function, it performs the swapping logic directly on 'arr'.
+
+#Print the swapped values of x and y
+print ("x:", arr[0]) #x: 10
+print ("y:", arr[1]) #y: 5
+
+#By passing a list in python and modify its value inside a function, 
+#the changes made to the list inside the function will be reflected in the original list
