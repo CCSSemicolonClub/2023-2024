53b9759db3ab13f152eb1672a9a83969d4d5a880
Semicolon Admin <147300109+ccssemicolon@users.noreply.github.com>
2023-10-19 22:43:02 +0800

    Update Basic.py

--- Python/Dailies/SwappingTwoVariables/Basic.py
+++ Python/Dailies/SwappingTwoVariables/Basic.py
@@ -1,3 +1,15 @@
+"""
+Required variables for Basic and Advance Categories:
+
+-   int x = 5;
+-   int y = 10;
+
+[Basic]
+           1. Create a program that swaps the values of two integers. 
+           Using any approach is allowed, as long as when you print x, 
+           it will display 10, and for y, it will display 5.
+"""
+
 x = 5
 y = 10
 
