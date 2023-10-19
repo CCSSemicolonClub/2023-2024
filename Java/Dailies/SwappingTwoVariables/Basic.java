a62e1ff8726e296590e5f7b65e5b2a2ebeddc9a1
Semicolon Admin <147300109+ccssemicolon@users.noreply.github.com>
2023-10-19 22:44:06 +0800

    Update Basic.java

--- Java/Dailies/SwappingTwoVariables/Basic.java
+++ Java/Dailies/SwappingTwoVariables/Basic.java
@@ -1,3 +1,14 @@
+/*
+Required variables for Basic and Advance Categories:
+
+-   int x = 5;
+-   int y = 10;
+
+[Basic]
+           1. Create a program that swaps the values of two integers. 
+           Using any approach is allowed, as long as when you print x, 
+           it will display 10, and for y, it will display 5.
+*/
 public class Basic {
     public static void main(String[] args) {
         int a = 5;
