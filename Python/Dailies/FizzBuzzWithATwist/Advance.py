68b9b3abf83f16549d4947ad59e446dd7f96a6d6
Semicolon Admin <147300109+ccssemicolon@users.noreply.github.com>
2023-10-20 23:38:01 +0800

    Update Advance.py

--- Python/Dailies/FizzBuzzWithATwist/Advance.py
+++ Python/Dailies/FizzBuzzWithATwist/Advance.py
@@ -5,6 +5,11 @@
 """
 
 for x in range(1,101):
+    """
+     The use of the ternary operator here allows the program to make decisions 
+     without using traditional if or else statements. 
+     Instead, it determines what to print based on the result of each condition
+    """
     print("FizzBuzz" if x % 3 == 0 and x % 5 == 0
     else "Fizz" if x % 3 == 0
     else "Buzz" if x % 5 == 0
