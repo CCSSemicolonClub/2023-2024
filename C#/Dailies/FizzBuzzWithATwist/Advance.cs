0fb9ebc7670d015760f918910fbc8f2391ab6cd2
Semicolon Admin <147300109+ccssemicolon@users.noreply.github.com>
2023-10-20 23:37:11 +0800

    fizzbuzz with a twist advance - cs

---
+++ C#/Dailies/FizzBuzzWithATwist/Advance.cs
@@ -0,0 +1,21 @@
+/*
+ Rules:
+    No conditional statement (if, else, etc.)
+    Any programming language is allowed to use
+*/
+using System;
+class HelloWorld {
+    static void Main() {
+        for(int x = 1; x <= 100; x++){
+          /*
+            The use of the ternary operator here allows the program to make decisions 
+            without using traditional if or else statements. 
+            Instead, it determines what to print based on the result of each condition
+          */
+            Console.WriteLine(x % 3 == 0 && x % 5 == 0 ? "FizzBuzz" : 
+            x % 3 == 0 ? "Fizz" :
+            x % 5 == 0 ? "Buzz" :
+            $"{x}");
+        }
+    }
+}
