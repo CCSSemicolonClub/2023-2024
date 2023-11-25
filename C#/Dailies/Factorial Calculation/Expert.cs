6993d042ab4f098a5f6c5765c97327feb23501e3
Semicolon Admin <147300109+ccssemicolon@users.noreply.github.com>
2023-11-25 08:06:37 +0800

    Create Expert.cs

---
+++ C#/Dailies/Factorial Calculation/Expert.cs
@@ -0,0 +1,58 @@
+using System;
+using System.Numerics;
+
+class HelloWorld
+{
+  /*
+    The question mark ? in BigInteger? mod indicates that the parameter mod is nullable, 
+    meaning it can accept a value of BigInteger or be null.
+
+    In C#, adding ? after a type declaration makes that type nullable. 
+    This allows a variable of that type to either hold a valid value of the specified type or a null value.
+
+   BigInteger is used to handle arbitrarily large integers that other numeric types cannot accommodate.
+   It allows working with very large numbers like factorials of large integers.
+     Function to calculate factorial modulo a given integer
+  */
+    static BigInteger Factorial(int n, BigInteger? mod = null)
+    {
+        if (n < 0)
+        {
+            Console.WriteLine("Invalid input (negative number)");
+            return -1; // Returning -1 for invalid input
+        }
+
+        BigInteger result = 1;
+
+        // Calculate factorial without considering modulo
+        for (int i = 1; i <= n; i++)
+        {
+            result *= i;
+
+            // If modulo is specified, take the remainder at each step
+            if (mod != null)
+            {
+                result %= mod.Value; // Using Value to access the non-nullable BigInteger
+            }
+        }
+
+        return result;
+    }
+
+    static void Main()
+    {
+        // Test cases for the factorial function
+        BigInteger resultSet1 = Factorial(100);
+        BigInteger resultSet2 = Factorial(200);
+        BigInteger resultSet3 = Factorial(1000, 1);
+        BigInteger resultSet4 = Factorial(1000, (BigInteger)(Math.Pow(10, 9)) + 7);
+        BigInteger resultSet5 = Factorial(1000, 100);
+
+        // Displaying the results
+        Console.WriteLine("Factorial of 100: " + resultSet1);
+        Console.WriteLine("Factorial of 200: " + resultSet2);
+        Console.WriteLine("Factorial of 1000: " + resultSet3);
+        Console.WriteLine("Factorial of 1000 (mod 10^9 + 7): " + resultSet4);
+        Console.WriteLine("Factorial of 1000 (mod 100): " + resultSet5);
+    }
+}
