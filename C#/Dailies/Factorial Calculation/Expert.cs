f5354e5a403e5300130a318cfac4845715520e98
Semicolon Admin <147300109+ccssemicolon@users.noreply.github.com>
2023-12-10 23:41:52 +0800

    Update Expert.cs

--- C#/Dailies/Factorial Calculation/Expert.cs
+++ C#/Dailies/Factorial Calculation/Expert.cs
@@ -1,3 +1,17 @@
+/*
+[Expert]
+result_set1 = factorial(100)
+result_set2 = factorial(200)
+result_set3 = factorial(1000, 1)
+result_set4 = factorial(1000, 10**9 + 7)
+result_set5 = factorial(1000, 100)
+     Rules:
+    Implement a program to calculate the factorial WITHOUT using built-in libraries like math.
+    Optimize the program to handle large factorials efficiently, taking into account performance and memory constraints.
+    Ensure the program works for calculating factorials of very large numbers, e.g., 200.
+    Implement the factorial calculation in a way that allows you to calculate the factorial of a number modulo a given integer.
+*/
+
 using System;
 using System.Numerics;
 
