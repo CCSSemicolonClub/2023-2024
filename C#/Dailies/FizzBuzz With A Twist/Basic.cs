43759743384f795a84c4570e1f3d57c7f4a454eb
Semicolon Admin <147300109+ccssemicolon@users.noreply.github.com>
2024-01-30 23:26:29 +0800

    Update Basic.cs

--- C#/Dailies/FizzBuzz With A Twist/Basic.cs
+++ C#/Dailies/FizzBuzz With A Twist/Basic.cs
@@ -9,21 +9,26 @@ Create a FizzBuzz program, which will print
 
 using System;
 
-class HelloWorld {
-    static void Main() {
+class HelloWorld 
+{
+    static void Main() 
+    {
         // Iterate through numbers from 1 to 100
-        for(int x=1; x <= 100; x++){
+        for(int x=1; x <= 100; x++)
+        {
             // Initialize an empty string to store the result
             string res = "";
             
             // Check if the current number is divisible by 3
-            if(x % 3 == 0){
+            if(x % 3 == 0)
+            {
                 // If true, add "Fizz" to the result
                 res += "Fizz";
             }
             
             // Check if the current number is divisible by 5
-            if(x % 5 == 0){
+            if(x % 5 == 0)
+            {
                 // If true, add "Buzz" to the result
                 res += "Buzz";
             }
